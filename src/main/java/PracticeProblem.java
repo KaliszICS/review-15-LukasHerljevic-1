16a2652968b7d478a85d3bd69a3c3186041f7990
LukasHerljevic <258844296+LukasHerljevic@users.noreply.github.com>
2026-04-27 17:47:01 +0000

    e

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,36 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static int goodString(String s) {
+        int n = s.length();
+        int maxGoodLength = -1;
 
-	}
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                
+                if (s.charAt(i) == s.charAt(j)) {
+                    int currentLength = j - i + 1;
+                    maxGoodLength = Math.max(maxGoodLength, currentLength);
+                }
+            }
+        }
 
+        
+        if (maxGoodLength == -1) {
+            return -1;
+        }
+
+        
+        return n - maxGoodLength;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(goodString("abcda"));          // Output: 0
+        System.out.println(goodString("abcdefghiab"));    // Output: 1
+        System.out.println(goodString("pqr"));            // Output: -1
+        System.out.println(goodString("bacdefghipalop")); // Output: 4
+    }
 }
+
+
